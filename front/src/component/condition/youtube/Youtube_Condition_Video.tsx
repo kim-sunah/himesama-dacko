688a7c4caf738e0e15bc7183a3b158641913a4c9
wqdsdsf123 <chlxodud04@naver.com>
2024-05-31 01:24:05 +0900

    ....

--- front/src/component/condition/youtube/Youtube_Condition_Video.tsx
+++ front/src/component/condition/youtube/Youtube_Condition_Video.tsx
@@ -80,7 +80,7 @@ export default function YoutubeConditionVideo() {
         fetchData()
       }
     
-    }, [search,PageToken])
+    }, [search,PageToken]);
     return (
         
       <div style={{ marginLeft: '10%', marginRight: '10%' }}>
