e85656b398b883060dfd33159b5c9eedf7a7f298
wqdsdsf <chlxodud04@naver.com>
2024-08-10 16:38:33 +0900

    .....

--- front/src/pages/main/Leaderboard.tsx
+++ front/src/pages/main/Leaderboard.tsx
@@ -50,28 +50,28 @@ export default function Leaderboard({ title, img, rankings }: LeaderboardProps) 
                 <table className="w-full table-fixed border-collapse " >
                     <thead className="bg-muted border-b">
                         <tr>
-                            <th className="w-1/4 px-4  text-center text-sm font-medium text-muted-foreground">이름</th>
-                            <th className="w-1/4 px-4  text-center text-sm font-medium text-muted-foreground "><span className="flex justify-center">구독수</span></th>
-                            <th className="w-1/4 px-4  text-center text-sm font-medium text-muted-foreground"><span className="flex justify-center">조회수</span></th>
-                            <th className="w-1/4 px-4  text-center text-sm font-medium text-muted-foreground">
+                            <th className="w-1/4 px-4  text-center text-base text-sm font-medium text-muted-foreground whitespace-nowrap">이름</th>
+                            <th className="w-1/4 px-4  text-center text-base text-sm font-medium text-muted-foreground whitespace-nowrap"><span className="flex justify-center">구독수</span></th>
+                            <th className="w-1/4 px-4  text-center text-base text-sm font-medium text-muted-foreground whitespace-nowrap"><span className="flex justify-center">조회수</span></th>
+                            <th className="w-1/4 px-4  text-center text-base text-sm font-medium text-muted-foreground whitespace-nowrap">
                                 <span className="flex justify-center"> {Tab === "today" ? "전일 대비(%)" : Tab === "week" ? "주간 대비(%)" : "월간 대비(%)"}</span>
 
                             </th>
                         </tr>
                     </thead>
                     <tbody >
-                        {rankings && rankings.slice(0, 4).map((Channel, index) => (
+                        {rankings && rankings.slice(0, 5).map((Channel, index) => (
                             <tr key={index} className="hover:bg-muted/50">
                                 <td className="font-medium" style={{marginLeft:"10%"}}>
                                     <Link to={`${Channel.Channel_Id}`} className="flex items-center gap-2">
                                         <img src={Channel.channel_img} width={35} alt={Channel.Channel_nickname} className="flex-shrink-0 border-r rounded-full" />
-                                        <span className="text-sm truncate text-black">{Channel.Channel_nickname}</span>
+                                        <span className="text-sm truncate text-black text-base">{Channel.Channel_nickname}</span>
                                     </Link>
                                 </td>
-                                <td className=" px-4 text-center text-muted-foreground">{formatNumber(Number(Channel.subscriberCount))} </td>
-                                <td className=" px-4  text-center text-muted-foreground">{formatNumber(Number(Channel.viewCount))}</td>
+                                <td className=" px-4 text-center text-muted-foreground text-base">{formatNumber(Number(Channel.subscriberCount))} </td>
+                                <td className=" px-4  text-center text-muted-foreground text-base">{formatNumber(Number(Channel.viewCount))}</td>
                                 <td
-                                    className=" px-4  text-center text-muted-foreground"
+                                    className=" px-4  text-center text-muted-foreground text-base"
                                     style={{
                                         color: (() => {
                                             let value;
